89a968292bc26c58af7016312e3e0b858125a900
wushuming <2445806031@qq.com>
2025-08-29 15:19:59 +0800

    运行失败

--- mcp-client/src/main/java/org/example/service/impl/DocumentServiceImpl.java
+++ mcp-client/src/main/java/org/example/service/impl/DocumentServiceImpl.java
@@ -2,6 +2,8 @@ package org.example.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.example.service.DocumentService;
+import org.example.utils.CustomTextSplitter;
+import org.springframework.ai.vectorstore.redis.RedisVectorStore;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.reader.TextReader;
 import org.springframework.core.io.Resource;
@@ -12,11 +14,21 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class DocumentServiceImpl implements DocumentService {
+    private final RedisVectorStore redisVectorStore;
+
     @Override
     public List<Document> loadText(Resource resource, String fileName) {
         TextReader textReader = new TextReader(resource);
         textReader.getCustomMetadata().put("fileName", fileName);
         List<Document> documentList = textReader.get();
+
+        CustomTextSplitter tokenTextSplitter = new CustomTextSplitter();
+        List<Document> list = tokenTextSplitter.apply(documentList);
+        System.out.println("list = " + list);
+
+        // 向量存储
+        redisVectorStore.add(list);
+
         return documentList;
     }
 }
